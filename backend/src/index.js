2fa425f6907317cfe8545992e3a5fd4ec9dd3a80
Luka <lukabrkovic@skiff.com>
2024-01-14 09:57:17 +0100

    FEAT(es6 modules) serving 404 and index page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,36 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+
+// user routes
 app.use("/api/v1/user", userRoutes);
 
+// static files
+app.use(express.static("views"));
+
+// html page
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+})
+
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Test error!');
+});
+
+// 404 error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'));
+});
 
 
 app.listen(3000, () => {
