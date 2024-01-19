e40b1d52c7eb8a67510fd8e6ef767c0d6eb339a5
Michael Monaghan <Nostromos@users.noreply.github.com>
2024-01-19 14:53:30 -0500

    Added skeleton for main componenets and some documentation. More to come.

---
+++ frontend/src/components/ScheduleCard/ScheduleCard.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+/**
+ * Component for showing each Item, location, time, length, user, and when they're available. 
+ * These cards make up the schedule slider and can probably be used elsewhere.
+ * 
+ * Parent: ScheduleSlider
+ * Location: Home Page
+ * 
+ * Props: 
+ * - Username
+ * - Timezone
+ * - Times Available
+ * 
+ */
+
+export default function ScheduleCard({ username, timezone, availableTimes }) { // TODO Replace these props with proper names when we get the data model figured out
+
+  return (
+    <>
+      { /* TODO Add ScheduleCard JSX */}
+    </>
+  );
+}
