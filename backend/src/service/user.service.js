ec62765dc0a2688abfc2969de7f25054465c0b36
Brandon Hazelton <hazeltonbw@gmail.com>
2024-01-22 02:34:57 -0800

    Change methods to static, default export class

--- backend/src/service/user.service.js
+++ backend/src/service/user.service.js
@@ -1,22 +1,24 @@
-import {db} from "../utils/db.js";
-import {users} from "../utils/schema.js";
-import {nanoid} from "nanoid";
-import argon2 from "argon2";
+import { nanoid } from 'nanoid';
+import argon2 from 'argon2';
+import db from '../utils/db.js';
+import users from '../utils/schema.js';
 
-export class UserService {
-    async insertUser({name, email, password}) {
-        const userPayload = {
-            id: nanoid(),
-            name,
-            email,
-            password,
-        }
-        return db.insert(users).values(userPayload).returning()
-    }
-    async hashPassword(password) {
-        return argon2.hash(password);
-    }
-    async verifyPassword(password, hash) {
-        return argon2.verify(hash, password);
-    }
+export default class UserService {
+  static async insertUser({ name, email, password }) {
+    const userPayload = {
+      id: nanoid(),
+      name,
+      email,
+      password,
+    };
+    return db.insert(users).values(userPayload).returning();
+  }
+
+  static async hashPassword(password) {
+    return argon2.hash(password);
+  }
+
+  static async verifyPassword(password, hash) {
+    return argon2.verify(hash, password);
+  }
 }
