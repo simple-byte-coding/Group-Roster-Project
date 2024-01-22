ef5d28cf55132ea2a1785c49e59d10938ad6a81a
simple-byte-coding <gagemorgan320@gmail.com>
2024-01-21 19:33:49 -0600

    Change name of backend folder to main since it contains the client files as well.

--- backend/drizzle.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    schema: "./src/utils/schema.js",
-    driver: 'pg',
-    dbCredentials: {
-        connectionString: "postgres://postgres:postgres@localhost:5432/postgres"
-    },
-    out: "./migrations",
-}
